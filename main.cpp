0519030423d043a9bc8825adfe4fc83e99147135
Sanskar-Awachar-commits <awacharsanskar@gmail.com>
2026-06-13 20:18:21 +0530

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+;
+int main() {
+    for (int i = 0; i < 100; i++) {
+        std::cout << i;
+        std::string apiKey = "Ax.iinvoDasmdgDijaondvimiaigpinabiufpoaidjmvooiawjdo";
+    }
+    return 0;
+}
