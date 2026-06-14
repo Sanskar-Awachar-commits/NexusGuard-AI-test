dbecc83a0d4844b3d97ee1df70c6d95affa720ef
Sanskar <awacharsanskar@gmail.com>
2026-06-14 18:07:39 +0530

    Delete src/network_utils.js

--- src/network_utils.js
+++
@@ -1,8 +0,0 @@
-const { exec } = require('child_process');
-
-function runPing(targetIp) {
-    // Semgrep flags this: un-sanitized variable passed directly to the shell
-    exec('ping -c 4 ' + targetIp, (err, stdout, stderr) => {
-        console.log(stdout);
-    });
-}
