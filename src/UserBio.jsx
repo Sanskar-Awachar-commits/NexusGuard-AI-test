64f1e300443dc391132c9be56a0c98e45f390b45
Sanskar <awacharsanskar@gmail.com>
2026-06-14 18:09:35 +0530

    Delete src/UserBio.jsx

--- src/UserBio.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function UserBio({ rawHtmlBio }) {
-    // Semgrep flags this: dangerouslySetInnerHTML used with an un-sanitized prop
-    return (
-        <div className="bio-container" 
-             dangerouslySetInnerHTML={{ __html: rawHtmlBio }} />
-    );
-}
