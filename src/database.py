73d89c890be672fb46205096c37b9138ac4fd31b
Sanskar <awacharsanskar@gmail.com>
2026-06-14 17:27:23 +0530

    Delete src/database.py

--- src/database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def fetch_user_data(username):
-    conn = sqlite3.connect('app.db')
-    cursor = conn.cursor()
-    # Semgrep flags this: user input is concatenated directly into the query (SQLi)
-    cursor.execute(f"SELECT * FROM users WHERE username = '{username}'")
-    return cursor.fetchall()
