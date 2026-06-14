706c8fe3db7abab1674828df15bba1f584f14b3f
Sanskar <awacharsanskar@gmail.com>
2026-06-14 17:25:28 +0530

    Delete src\database.py

--- src\database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import text
-
-def execute_query(db_session, query, params=None):
-    if params is None:
-        params = {}
-    query_text = text(query)
-    result = db_session.execute(query_text, params)
-    return result
