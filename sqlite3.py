ddcd00f4e95adb7f87dabea4b13c0c1110b88ec6
Sanskar-Awachar-commits <awacharsanskar@gmail.com>
2026-06-14 14:59:02 +0530

    apiKey

--- sqlite3.py
+++ sqlite3.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 
 gine = create_engine('sqlite:///example.db')
-env = "SUPER_SECRET_KEY"
+API_KEY = "SUPER_SECRET_KEY"
 with engine.connect() as connection:
     result = connection.execute(text('SELECT * FROM users WHERE name = :name'), {'name': 'John'})
     for row in result:
